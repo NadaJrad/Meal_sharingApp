abd04a1371d4230601013131edfd10f32b84bd75
NadaJrad <nadajradbenaicha@gmail.com>
2024-09-28 17:23:30 +0200

    node.js part 1 APIs done

--- api/src/index.js
+++ api/src/index.js
@@ -24,8 +24,82 @@ apiRouter.get("/", async (req, res) => {
 // This nested router example can also be replaced with your own sub-router
 apiRouter.use("/nested", nestedRouter);
 
+app.get("/my-route", (req, res) => {
+  res.send("helloo friend");
+});
+// FUTURE MEAL
+app.get("/future-meals", async (req, res) => {
+  try {
+    const result = await knex.raw("SELECT * FROM Meal WHERE `when` > NOW();");
+
+    const futureMeals = result[0] || [];
+
+    res.json(futureMeals);
+  } catch (error) {
+    console.error('Error retrieving future meals:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// PAST MEAL
+app.get("/past-meals", async (req, res) => {
+  try {
+    const result = await knex.raw("SELECT * FROM Meal WHERE `when` < NOW();");
+
+    const futureMeals = result[0] || [];
+
+    res.json(futureMeals);
+  } catch (error) {
+    console.error('Error retrieving future meals:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// all-meals
+app.get("/all-meals", async (req, res) => {
+  try {
+    const result = await knex.raw("SELECT * FROM Meal ORDER BY id;");
+
+    const futureMeals = result[0] || [];
+
+    res.json(futureMeals);
+  } catch (error) {
+    console.error('Error retrieving future meals:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+// first-meals
+app.get("/first-meal", async (req, res) => {
+  try {
+    const result = await knex.raw("SELECT * FROM Meal ORDER BY id ASC LIMIT 1;");
+
+    const futureMeals = result[0] || [];
+
+    res.json(futureMeals);
+  } catch (error) {
+    console.error('Error retrieving future meals:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+// last-meals
+app.get("/last-meal", async (req, res) => {
+  try {
+    const result = await knex.raw("SELECT * FROM Meal ORDER BY id DESC LIMIT 1;");
+
+    const futureMeals = result[0] || [];
+
+    res.json(futureMeals);
+  } catch (error) {
+    console.error('Error retrieving future meals:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+
 app.use("/api", apiRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`API listening on port ${process.env.PORT}`);
 });
+
